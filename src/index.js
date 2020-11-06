d00f53b4e52cd7758c6998f02c0fb9c53033c80b
stacksonstack <staceycarrillo44@gmail.com>
2020-11-06 15:18:07 -0500

    core deliverables done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form")
+  let newTask = document.getElementById("new-task-description")
+  let taskList = document.getElementById("tasks")
+
+
+
+  form.addEventListener("submit", (e) => {
+      e.preventDefault()
+      const li = document.createElement("li")
+      li.textContent = newTask.value  
+      taskList.append(li)
+  })
 });
